13832153b5cc79a363b93b3c19c3ccbd4bd57bae
Igor Knop <igorknop@gmail.com>
2016-06-17 19:19:13 -0300

    Inicia projeto java jpa na web

---
+++ src/java/br/cesjf/lppo/controllers/LivroController.java
@@ -0,0 +1,107 @@
+package br.cesjf.lppo.controllers;
+
+import br.cesjf.lppo.Livro;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.Query;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.transaction.UserTransaction;
+
+/**
+ *
+ * @author igor
+ */
+@WebServlet(name = "LivroController", urlPatterns = {"/index.html"})
+public class LivroController extends HttpServlet {
+
+    @PersistenceUnit(unitName = "jpa-web-pu")
+    EntityManagerFactory emf;
+
+    @Resource(name = "java:comp/UserTransaction")
+    UserTransaction ut;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Livros</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Livros</h1>");
+            //EntityManagerFactory emf = Persistence.createEntityManagerFactory("lppo-jpa-bas-pu");
+
+            EntityManager em = emf.createEntityManager();
+            Query q = em.createQuery("SELECT l FROM Livro l");
+            List<Livro> livros = q.getResultList();
+
+            for (Livro livro : livros) {
+                out.println("<li>" + livro + "</li>");
+            }
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
